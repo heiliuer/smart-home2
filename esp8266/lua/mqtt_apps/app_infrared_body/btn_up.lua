b52f9443cbf6afd2da9e648e346f50a98310fabe
heiliuer <eee>
2017-03-07 23:05:36 +0800

    fixed

---
+++ esp8266/lua/mqtt_apps/app_infrared_body/btn_up.lua
@@ -0,0 +1,29 @@
+local moduleName = ...
+local M = {}
+_G[moduleName] = M
+
+
+local function debounce(func)
+    local last = 0
+    local delay = 1000000
+
+    return function(...)
+        local now = tmr.now()
+        if now - last < delay then return end
+
+        last = now
+        return func(...)
+    end
+end
+
+function M.onPress(buttonPin, onChange)
+    gpio.mode(buttonPin, gpio.INT, gpio.PULLUP)
+    gpio.trig(buttonPin, "up", debounce(function()
+        if gpio.read(buttonPin) == 1 then
+            onChange()
+            tmr.delay(500000)
+        end
+    end))
+end
+
+return M
